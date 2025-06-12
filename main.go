cb53d9b6875ecc63cffe8f8ba2d0f242e38a48be
jupiter-commits <192867955+jupiter-commits@users.noreply.github.com>
2025-06-12 16:44:05 +0300

    Add HandleRequest

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 
+	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/gin-gonic/gin"
 )
 
@@ -15,4 +17,16 @@ func init() {
 		func(c *gin.Context) {
 			c.JSON(http.StatusOK, gin.H{"message": "Welcome"})
 		})
+}
+
+type MyEvent struct {
+	Name string `json:"name"`
+}
+
+func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
+	return fmt.Sprintf("Hello, %$", name.Name), nil
+}
+
+func main() {
+	lambda.Start(HandleRequest)
 }
